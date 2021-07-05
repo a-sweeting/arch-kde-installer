d89ab12bd7bea051a49ac8713c9d60589f107e5d
Anthony <anthony.sweeting316@gmail.com>
2021-07-05 13:33:53 +0100

    Minor edits

--- arch_install_kde1.sh
+++ arch_install_kde1.sh
@@ -44,4 +44,63 @@ echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "chroot into new system"
-arch-chroot /mnt
+arch-chroot /mnt << EOF
+
+echo "Setting timezone to Europe/London"
+ln -sf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
+
+echo "Setting hardware clock"
+hwclock --systohc
+
+echo "Set locale info"
+cp /etc/locale.gen /etc/locale.gen.bkp
+sed "s/#$LANGUAGE/$LANGUAGE/" /etc/locale.gen > /etc/locale.gen.tmp
+mv /etc/locale.gen.tmp /etc/locale.gen
+locale-gen
+
+echo "Setting Language and keyboard"
+echo "LANG=$LANGUAGE" > /etc/locale.conf
+echo "KEYMAP=$KEYBOARD" > /etc/vconsole.conf
+
+echo "Setting hostname"
+
+echo "$HOSTNAME" > /etc/hostname
+echo "127.0.0.1		localhost" > /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1		${HOSTNAME}.localdomain	${HOSTNAME}" >> /etc/hosts
+
+echo "Adding user"
+
+echo -e $ROOT_PASSWD"\n"$ROOT_PASSWD | passwd
+echo -e $NEW_USER_PASSWD"\n"$NEW_USER_PASSWD | passwd $NEW_USER
+
+echo "Adding user to groups"
+
+usermod -aG wheel,audio,video,optical,storage $NEW_USER
+cp /etc/sudoers /etc/sudoers.bkp
+sed "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers > /etc/sudoers.tmp
+mv /etc/sudoers.tmp /etc/sudoers
+
+echo "Setting up grub"
+
+pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+
+mkdir /boot/EFI
+mount "${DISK}1" /boot/EFI
+
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Installing vim"
+pacman -S --noconfirm vim
+
+echo "Installing Network Manager"
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+exit 
+EOF
+
+echo "Unmounting partitions"
+umount -l /mnt
+reboot
--- arch_install_kde2.sh
+++ arch_install_kde2.sh
@@ -12,9 +12,6 @@ ROOT_PASSWD="1234"
 NEW_USER="test"
 NEW_USER_PASSWD="1234"
 
-echo "Installing vim"
-pacman -S vim
-
 echo "Setting timezone to Europe/London"
 ln -sf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
 
@@ -23,7 +20,7 @@ hwclock --systohc
 
 echo "Set locale info"
 cp /etc/locale.gen /etc/locale.gen.bkp
-sed 's/#en_GB/en_GB/' /etc/locale.gen > /etc/locale.gen.tmp
+sed "s/#$LANGUAGE/$LANGUAGE/" /etc/locale.gen > /etc/locale.gen.tmp
 mv /etc/locale.gen.tmp /etc/locale.gen
 locale-gen
 
@@ -38,6 +35,33 @@ echo "127.0.0.1		localhost" > /etc/hosts
 echo "::1		localhost" >> /etc/hosts
 echo "127.0.1.1		${HOSTNAME}.localdomain	${HOSTNAME}" >> /etc/hosts
 
+echo "Adding user"
+
 echo -e $ROOT_PASSWD"\n"$ROOT_PASSWD | passwd
 echo -e $NEW_USER_PASSWD"\n"$NEW_USER_PASSWD | passwd $NEW_USER
+
+echo "Adding user to groups"
+
 usermod -aG wheel,audio,video,optical,storage $NEW_USER
+cp /etc/sudoers /etc/sudoers.bkp
+sed "s/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/" /etc/sudoers > /etc/sudoers.tmp
+mv /etc/sudoers.tmp /etc/sudoers
+
+echo "Setting up grub"
+
+pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+
+mkdir /boot/EFI
+mount "${DISK}1" /boot/EFI
+
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Installing vim"
+pacman -S --noconfirm vim
+
+echo "Installing Network Manager"
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+exit
