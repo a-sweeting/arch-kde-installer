7625237d46c4de2c5f12dde37c844b1bf52e912f
Anthony <anthony.sweeting316@gmail.com>
2021-07-05 10:58:22 +0100

    Minor edits

--- arch_install_kde1.sh
+++ arch_install_kde1.sh
@@ -45,27 +45,3 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 echo "chroot into new system"
 arch-chroot /mnt
-
-echo "Installing vim"
-pacman -S vim
-
-echo "Setting timezone to Europe/London"
-ln -sf "/usr/share/zoneinfo/${TIMEZONE}" /etc/localtime
-
-echo "Setting hardware clock"
-hwclock --systohc
-
-echo "Set locale info"
-vim /etc/locale.gen
-locale-gen
-
-echo "Setting Language and keyboard"
-echo "LANG=${LANGUAGE}" > /etc/locale.conf
-echo "KEYMAP=$KEYBOARD" > /etc/vconsole.conf
-
-echo "Setting hostname"
-
-echo "$HOSTNAME" > /etc/hostname
-echo "127.0.0.1		localhost" > /etc/hosts
-echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1		${HOSTNAME}.localdomain	${HOSTNAME}" >> /etc/hosts
